cc7cf888fb0d8489dd9513f232a7cd16650b52f2
Shikina Cabral <130032168+CShikina@users.noreply.github.com>
2023-11-29 19:19:46 +0800

    Programmed a FizzBuzz game

---
+++ submissions/challenges/challenge-day6.py
@@ -0,0 +1,12 @@
+#FizzBuzz Game
+limit = int(input("Please enter the limit: "))
+
+for i in range(1, limit +1):
+    if (i % 5 == 0 and i % 3 == 0):
+        print("FizzBuzz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    else:
+        print(i)
